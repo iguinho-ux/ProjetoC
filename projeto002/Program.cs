8e244343ecfa55b0136a19652796ed49955c8f6c
iguinho-ux <igormachadosr@gmail.com>
2025-04-04 09:46:33 -0300

    projeto01

---
+++ projeto002/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Qual a sua idade?"); '2';
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        if (idade >= 18)
+            Console.WriteLine("Você é maior de idade.");
+        else
+            Console.WriteLine("Você é menor de idade.");
+    }
+}
